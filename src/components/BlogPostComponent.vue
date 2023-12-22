a47a3cfbb993cb960b843aa55dd77a89665a7622
Fannn03 <fann.fann771@gmail.com>
2023-12-22 20:57:02 +0700

    passing props to child component

---
+++ src/components/BlogPostComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  interface BlogInterface {
+    title: string,
+    author?: string,
+    category: string[] | [],
+    description: string
+  }
+
+  const props = withDefaults(defineProps<BlogInterface>(), {
+    title: 'Untitled',
+    category: () => [],
+    description: 'adadwa'
+  })
+</script>
+
+<template>
+  <main>
+    Blog Post : {{ props.title }}
+  </main>
+</template>
---
+++ src/views/PostView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import BlogPostComponent from "@/components/BlogPostComponent.vue";
+
+</script>
+
+<template>
+  <main>
+    <h1>Hello world</h1>
+    <BlogPostComponent
+      title="My title"
+    />
+  </main>
+</template>
