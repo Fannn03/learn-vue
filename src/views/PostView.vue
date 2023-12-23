e39343e280202c596f8fe9031a5836cf64d115f6
Fannn03 <fann.fann771@gmail.com>
2023-12-23 18:46:02 +0700

    same

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
   import BlogPostComponent from "@/components/BlogPostComponent.vue";
+  import { ref } from 'vue';
 
+  const count = ref(0);
 </script>
 
 <template>
   <main>
     <h1>Hello world</h1>
+    <p>Total count :  {{ count }}</p>
     <BlogPostComponent
+      @add-count="count++"
       title="My title"
     />
   </main>
