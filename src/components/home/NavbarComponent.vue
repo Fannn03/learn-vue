e84375df5f91ceb68997f11356108b64f7c02678
Fannn03 <fann.fann771@gmail.com>
2023-12-24 14:26:58 +0700

    add button back to top

--- src/components/home/NavbarComponent.vue
+++ src/components/home/NavbarComponent.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-  import { onMounted, ref, type Ref } from 'vue';
 
-  let isScrolled: Ref<boolean> = ref(false);
-  const onScroll = (event: Event) => {
-    if(window.scrollY > 100) isScrolled.value = true;
-    else isScrolled.value = false;
-  }
-  
-  onMounted(() => {
-    window.addEventListener('scroll', onScroll);
-  })
 </script>
 
 <template>
-  <div class="md:flex md:flex-row md:justify-between md:px-24 md:py-4 transition-colors duration-300 text-white font-dm-serif"
-  :class="{'bg-gray-900': isScrolled}"
-  >
+  <div class="md:flex md:flex-row md:justify-between md:px-24 md:py-4 transition-colors duration-300 text-white font-dm-serif">
     <router-link to="/" class="md:text-3xl hover:text-blue-500 transition-colors duration-300">
       Portfolio
     </router-link>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
   import NavbarComponent from '@/components/home/NavbarComponent.vue';
+  import { onMounted, ref, type Ref } from 'vue';
+
+  let isScrolled: Ref<boolean> = ref(false);
+
+  const onScroll = () => {
+    if(window.scrollY > 100) isScrolled.value = true;
+    else isScrolled.value = false;
+  }
+
+  const backToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
+  onMounted(() => {
+    window.addEventListener('scroll', onScroll);
+  })
 </script>
 
 <template>
   <div class="text-white">
     <NavbarComponent
       class="top-0 sticky"
+      :class="{'bg-gray-900': isScrolled}"
     />
     <!-- Body -->
     <div class="md:flex md:flex-col md:gap-36 md:px-32 md:py-12">
@@ -17,7 +34,7 @@
           <p class="text-gray-400 md:mt-6">An Junior backend engineer that passioned in <span class="text-blue-500">Node JS</span></p>
         </div>
         <img src="../assets/profile.jpeg" alt="profile"
-        class="p-2 rounded-full md:w-1/3"
+        class="md:w-1/3 rounded-full transition delay-100 hover:scale-105"
         >
       </div>
       <!-- End Header -->
@@ -25,7 +42,29 @@
       <div class="md:flex md:flex-col">
         <h1 class="md:text-4xl text-center font-dm-serif text-gray-400">About</h1>
       </div>
+      <div class="md:flex md:flex-col">
+        <h1 class="md:text-4xl text-center font-dm-serif text-gray-400">About</h1>
+      </div>
+      <div class="md:flex md:flex-col">
+        <h1 class="md:text-4xl text-center font-dm-serif text-gray-400">About</h1>
+      </div>
+      <div class="md:flex md:flex-col">
+        <h1 class="md:text-4xl text-center font-dm-serif text-gray-400">About</h1>
+      </div>
       <!-- End About -->
     </div>
+    <!-- End Body -->
   </div>
+
+    <!-- Button Up -->
+    <div class="bottom-5 right-5 transition ease-in-out duration-300"
+    :class="[isScrolled ? 'fixed' : 'hidden']"
+    >
+      <button @click="backToTop" class="p-3 bg-blue-500 text-white rounded-md transition duration-300 hover:scale-105">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+        </svg>
+      </button>
+    </div>
+    <!-- End Button Up -->
 </template>
